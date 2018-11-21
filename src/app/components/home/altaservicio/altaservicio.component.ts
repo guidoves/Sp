de3ddcb7d9d17d3d2100ec6906a1ec38f30bdb58
guido <guido@tokkolabs.com>
2018-11-21 14:40:50 -0300

    punto 3-4

--- src/app/components/home/altaservicio/altaservicio.component.ts
+++ src/app/components/home/altaservicio/altaservicio.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-altaservicio',
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltaservicioComponent implements OnInit {
 
-  constructor() { }
+  @Output() answer: EventEmitter<any>;
+
+  form: FormGroup;
+
+  constructor() {
+    this.answer = new EventEmitter();
+
+    this.form = new FormGroup(
+        {
+          'name': new FormControl('', [Validators.required, Validators.minLength(1)]),
+          'storage': new FormControl('', [Validators.required, Validators.pattern('^[0-9]{1,100}$')])
+        }
+    );
+
+  }
+
+  newServ() {
+     const body = this.form.value;
+
+      this.answer.emit(body);
+
+  }
 
   ngOnInit() {
   }
